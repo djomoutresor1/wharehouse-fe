93e91468897ae7f17478811db3df11ab63224b58
djomoutresor <djomoutresor1@hotmail.fr>
2022-01-27 06:37:44 +0100

    git(aggiornamento master)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CorsiaAComponent } from './corsia/corsia-a/corsia-a.component';
-import { CorsiaBComponent } from './corsia/corsia-b/corsia-b.component';
-import { CorsiaCComponent } from './corsia/corsia-c/corsia-c.component';
-import { CorsiaDComponent } from './corsia/corsia-d/corsia-d.component';
-import { CorsiaEComponent } from './corsia/corsia-e/corsia-e.component';
-import { CorsiaFComponent } from './corsia/corsia-f/corsia-f.component';
-import { CorsiaGComponent } from './corsia/corsia-g/corsia-g.component';
-import { CorsiaHComponent } from './corsia/corsia-h/corsia-h.component';
-import { CorsiaIComponent } from './corsia/corsia-i/corsia-i.component';
-import { CorsiaJComponent } from './corsia/corsia-j/corsia-j.component';
-import { CorsiaKComponent } from './corsia/corsia-k/corsia-k.component';
-import { CorsiaLComponent } from './corsia/corsia-l/corsia-l.component';
-import { CorsiaMComponent } from './corsia/corsia-m/corsia-m.component';
-import { CorsiaNComponent } from './corsia/corsia-n/corsia-n.component';
+
 import { CorsiaPComponent } from './corsia/corsia-p/corsia-p.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { FooterComponent } from './footer/footer/footer.component';
@@ -28,25 +15,11 @@ import { SingleCorsiaComponent } from './corsia/single-corsia/single-corsia.comp
 @NgModule({
   declarations: [
     AppComponent,
-    CorsiaAComponent,
-    CorsiaBComponent,
-    CorsiaCComponent,
-    CorsiaDComponent,
-    CorsiaEComponent,
-    CorsiaFComponent,
-    CorsiaGComponent,
-    CorsiaHComponent,
-    CorsiaIComponent,
-    CorsiaJComponent,
-    CorsiaKComponent,
-    CorsiaLComponent,
-    CorsiaMComponent,
-    CorsiaNComponent,
     CorsiaPComponent,
     DashboardComponent,
     FooterComponent,
-    HeaderComponent,,
-    CorsiaTableComponent
+    HeaderComponent,
+    CorsiaTableComponent,
     SingleCorsiaComponent
 
   ],
