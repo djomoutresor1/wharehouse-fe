80f440438cd8d26e37101f79ae1180843db90620
djomoutresor <djomoutresor1@hotmail.fr>
2022-01-27 05:58:10 +0100

    git(rebase after merge)

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 
+
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss','../../app.component.scss']
+  styleUrls: ['./dashboard.component.scss', '../../app.component.scss'],
 })
 export class DashboardComponent extends AppComponent implements OnInit {
+
+   alphabet = Array.from(Array(15)).map((e, i) => i + 65);
+   corsie = this.alphabet.map((x) => String.fromCharCode(x));
+
 
   constructor() {
     super();
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
