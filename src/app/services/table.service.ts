aaebddd51a11c1a0ed50f1c84f27b863592788e6
djomoutresor <djomoutresor1@hotmail.fr>
2022-02-02 11:09:19 +0100

    git(1 commit:popolate table)

---
+++ src/app/services/table.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LaneModel } from 'src/model/corsia/lane-model';
+import { LanesModel } from 'src/model/corsia/lanes-model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TableService {
+  data: LaneModel[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  public getDataTable() {
+    return this.http.get<LanesModel>("../assets/mocks/entireDataMock.json");
+  }
+
+  ngOnInit(): void {
+    this.getDataTable().subscribe((response) => {
+      console.log("response: ", response);
+      this.data = response.lanes;
+      console.log("dataTable: ", response.lanes);
+    });
+  }
+  // getDataTables(){
+  //   this.getDataTable().subscribe((response) => {
+  //     console.log("response: ", response);
+  //     this.data = response.lanes;
+  //     console.log("dataTable: ", response.lanes);
+  //   });
+  // }
+
+}
---
+++ src/app/services/tables.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LaneModel } from 'src/model/corsia/lane-model';
+import { RowModel } from 'src/model/corsia/row-model';
+import { ShelfModel } from 'src/model/corsia/shelf-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TablesService {
+
+  constructor(private http: HttpClient) { }
+
+  public getDataTable() {
+    return this.http.get<any>("../assets/mocks/entireDataMock.json");
+  }
+
+  
+}
