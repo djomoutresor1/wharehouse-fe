df619b1c80ced17c8241289825b32b219c75973d
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2022-02-04 23:28:08 +0100

    refactoring and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
-import { FooterComponent } from './footer/footer/footer.component';
-import { HeaderComponent } from './header/header/header.component';
-import { CorsiaTableComponent } from './table/corsia-table/corsia-table.component';
-import { RackTableComponent } from './table/rack-table/rack-table.component';
-import { EditTableComponent } from './table/edit-table/edit-table.component';
 import { DashboardService } from './services/dashboard.service';
-import { VerticalLaneComponent } from './composants/vertical-lane/vertical-lane.component';
-import { HorizontalLaneComponent } from './composants/horizontal-lane/horizontal-lane.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RackTableComponent,
-    DashboardComponent,
-    FooterComponent,
-    HeaderComponent,
-    CorsiaTableComponent,
-    EditTableComponent,
-    VerticalLaneComponent,
-    HorizontalLaneComponent,
+  declarations: [AppComponent],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    AntDesignComponentsModule,
+    SharedModule,
   ],
-  imports: [HttpClientModule, BrowserModule, AppRoutingModule, AntDesignComponentsModule],
   providers: [DashboardService],
   bootstrap: [AppComponent],
 })
--- src/app/modules/ant-design-components.module.ts
+++ src/app/modules/ant-design-components.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from "@angular/core";
-import { NzButtonModule } from "ng-zorro-antd/button";
+import { NgModule } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzResultModule } from 'ng-zorro-antd/result';
 
 @NgModule({
-    exports: [NzButtonModule]
+  exports: [NzButtonModule, NzIconModule, NzResultModule],
 })
 export class AntDesignComponentsModule {}
