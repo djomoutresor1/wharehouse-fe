d6d2a3639768bef6ff38c62756c7eab42443242c
djomoutresor <djomoutresor1@hotmail.fr>
2022-03-12 10:00:35 +0100

    git(prova retrieve data of json)

--- src/app/pages/dashboard/dashboard-rack-detail/dashboard-rack-detail.component.ts
+++ src/app/pages/dashboard/dashboard-rack-detail/dashboard-rack-detail.component.ts
@@ -14,23 +14,21 @@ import { ShelfModel } from 'src/model/corsia/shelf-model';
   templateUrl: './dashboard-rack-detail.component.html',
   styleUrls: ['./dashboard-rack-detail.component.scss'],
 })
-export class DashboardRackDetailComponent implements  OnInit {
-
+export class DashboardRackDetailComponent implements OnInit {
   rackName: any;
-  rackNumber:any;
+  rackNumber: any;
   lanes: LaneModel[] = [];
   resultRackName: any;
+  s: any;
 
-  resultshelves: any;
-
-
-
-  constructor(private router: Router, private route: ActivatedRoute,private tableService: TableService) {
-     
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private tableService: TableService
+  ) {
     console.log('router: ', this.router);
     this.rackName = this.route.snapshot.paramMap.get(PathParams.RACK_NAME);
     console.log('rackName: ', this.rackName);
-
   }
 
   ngOnInit(): void {
@@ -39,35 +37,56 @@ export class DashboardRackDetailComponent implements  OnInit {
     this.rackNumber = this.route.snapshot.paramMap.get(PathParams.RACK_NUMBER);
 
     this.tableService.getDataTable().subscribe((response) => {
-      console.log("response2: ", response.lanes);
+      console.log('response2: ', response.lanes);
       response.lanes.map((x) => {
-        if(x.name === this.rackName.toLocaleUpperCase()) {
-          this.resultRackName = x.rows.filter((y) => y.row.toString() === this.rackNumber);
+        if (x.name === this.rackName.toLocaleUpperCase()) {
+          this.resultRackName = x.rows.filter(
+            (y) => y.row.toString() === this.rackNumber
+          );
         }
       });
       let b = this.resultRackName[0].shelves;
-      b.map(function (value: ShelfModel) {
-        console.log("positions: ",value.positions);
+      let a0 = b[0].positions;
+      let a1 = b[1].positions;
+      let a2 = b[2].positions;
+      let a3 = b[3].positions;
 
-        let c = value.positions;
-        c.map(function (value: PositionModel) {
-          console.log("dimensions: ",value.dimensions);
-          console.log("length: ",value.dimensions.length);
-          console.log("width: ",value.dimensions.width);
-          console.log("depth: ",value.dimensions.depth);
+      console.log('a0: ', a0);
+      console.log('a1: ', a1);
+      console.log('a2: ', a2);
+      console.log('a3: ', a3);
+
+      const result0: any = [];
+      a0.forEach((x0: any) => {
+        result0.push(x0.dimensions.length);
+        result0.push(x0.dimensions.width);
+        result0.push(x0.dimensions.depth);
+      });
+
+      const result1: any = [];
+      a1.forEach((x1: any) => {
+        result1.push(x1.dimensions.length);
+        result1.push(x1.dimensions.width);
+        result1.push(x1.dimensions.depth);
+      });
+
+      const result2: any = [];
+      a2.forEach((x2: any) => {
+        result2.push(x2.dimensions.length);
+        result2.push(x2.dimensions.width);
+        result2.push(x2.dimensions.depth);
+      });
+
+      const result3: any = [];
+      a3.forEach((x3: any) => {
+        result3.push(x3.dimensions.length);
+        result3.push(x3.dimensions.width);
+        result3.push(x3.dimensions.depth);
       });
     });
-
-
-       console.log('resultRackName: ',this.resultRackName ); 
-       console.log('shelvese: ',this.resultRackName[0].shelves ); 
-       
-    });
-    
   }
 
   handleOnNavigate(url: String) {
-
-   this.router.navigate([`${Pages.WAREHOUSE}/${url}`]);
+    this.router.navigate([`${Pages.WAREHOUSE}/${url}`]);
   }
 }
