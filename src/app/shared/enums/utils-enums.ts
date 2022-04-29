4cfb4d3c5490f83d3f7d3644b7d34d57fcb72a1c
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2022-04-29 22:39:17 +0200

    Implement Remember me session

--- src/app/shared/enums/utils-enums.ts
+++ src/app/shared/enums/utils-enums.ts
@@ -2,6 +2,7 @@ export enum Utils {
   WAREHOUSE_ROOT_ENDPOINT = "/v1/warehouse",
   WAREHOUSE_JWT_TOKEN = "warehouse-jwt-token",
   WAREHOUSE_TIMEOUT_TOKEN = "timeout-token",
+  WAREHOUSE_REMEMBER_ME = "warehouse-remember-me",
   ADMIN = 'admin',
   PASSWORD = 'Qwerty84.',
 }
