3e51806664432d3ef9236fb5b4c0ccfc8d0107da
djomoutresor <djomoutresor1@hotmail.fr>
2022-06-09 17:25:42 +0200

    git(modification)

--- src/app/shared/interceptors/common.interceptor.ts
+++ src/app/shared/interceptors/common.interceptor.ts
@@ -38,7 +38,6 @@ export class CommonInterceptor implements HttpInterceptor {
     if (
       !request.url.endsWith(Auth.WAREHOUSE_LOGIN_USER) &&
       !request.url.endsWith(Auth.WAREHOUSE_REGISTER_USER) &&
-      !request.url.includes(Auth.WAREHOUSE_DELETE_USER) &&
       !request.url.includes(Auth.WAREHOUSE_FORGOT_PASSWORD) &&
       !request.url.includes(Auth.WAREHOUSE_VERIFY_LINK) &&
       !request.url.includes(Auth.WAREHOUSE_RESET_PASSWORD) &&
