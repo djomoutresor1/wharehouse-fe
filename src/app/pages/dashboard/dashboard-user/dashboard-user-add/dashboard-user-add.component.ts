9fc669072cf0a44de3701f795bb690130706db40
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2022-06-16 23:39:04 +0200

    Layout insert new user

--- src/app/pages/dashboard/dashboard-user/dashboard-user-add/dashboard-user-add.component.ts
+++ src/app/pages/dashboard/dashboard-user/dashboard-user-add/dashboard-user-add.component.ts
@@ -9,6 +9,7 @@ import { AlertType } from 'src/app/shared/enums/alert-type-enums';
 import { Pages } from 'src/app/shared/enums/pages-enums';
 import { WarehouseLocalStorage } from 'src/app/utils/warehouse-local-storage';
 import { BreadcrumbItemsModel } from 'src/model/utils/breadcrumb-items-model';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'warehouse-dashboard-user-add',
@@ -22,14 +23,20 @@ export class DashboardUserAddComponent implements OnInit {
   isAuth: boolean = false;
   alertType: string = '';
   messageAlert: string = '';
-  countryAndFlagData: any;
-  countryStatesData: any;
+  countryAndFlagData: any[] = [];
+  countryStatesData: any[] = [];
+  prefixPhoneData: any[] = [];
   showbuttonUpload: boolean = false;
   countrySelected: string = '';
+  stateSelected: string = '';
+  landlinePrefixSelected: string = '';
   countryDialCode: string = '';
   imgURL: any;
   selectedFile: any;
   dateFormat = 'dd/MM/YYYY';
+  okText: string = '';
+  descriptionAlert: string = '';
+  isExpiredToken: boolean = false;
 
   rolesList = [
     { label: 'Admin', value: 'admin' },
@@ -59,7 +66,7 @@ export class DashboardUserAddComponent implements OnInit {
   ngOnInit(): void {
     this.initForm();
     this.initComponent();
-    this.getWorldCountries();
+    this.getCountriesAndPrefixPhoneWorld();
   }
 
   initForm() {
@@ -73,7 +80,7 @@ export class DashboardUserAddComponent implements OnInit {
         [Validators.required, Validators.min(5), Validators.max(15)],
       ],
       email: [null, [Validators.required, Validators.email]],
-      secondEmail: [null, [Validators.required, Validators.email]],
+      secondEmail: [null, [Validators.email]],
       role: [null, [Validators.required]],
       gender: [null, [Validators.required]],
       image: '',
@@ -87,10 +94,10 @@ export class DashboardUserAddComponent implements OnInit {
           Validators.maxLength(10),
         ],
       ],
+      landlinePrefix: [null],
       landlineNumber: [
         '',
         [
-          Validators.required,
           Validators.pattern('^[0-9]*$'),
           Validators.minLength(6),
           Validators.maxLength(10),
@@ -104,7 +111,6 @@ export class DashboardUserAddComponent implements OnInit {
           Validators.pattern('^[0-9]*$'),
           Validators.minLength(3),
           Validators.maxLength(6),
-          Validators.required,
         ],
       ],
       address: ['', [Validators.required]],
@@ -134,10 +140,15 @@ export class DashboardUserAddComponent implements OnInit {
     };
   }
 
-  getWorldCountries() {
+  getCountriesAndPrefixPhoneWorld() {
     this.flagService.getDialCodeAndCountryFlag().subscribe(
       (response: { data: any }) => {
         this.countryAndFlagData = response.data;
+        this.prefixPhoneData = response.data?.filter((prefix: any) => {
+          return prefix?.dialCode !== undefined && prefix?.dialCode !== ' ';
+        });
+        // Only take the unique prefix phone
+        this.prefixPhoneData = _.uniqWith(this.prefixPhoneData, _.isEqual);
       },
       (error: HttpErrorResponse) => {
         console.log('enable to retrieve data country and flag ' + error);
@@ -224,8 +235,8 @@ export class DashboardUserAddComponent implements OnInit {
     }
   }
 
-  getCountryDialCode(): string {
-    return this.countryDialCode.includes('+')
+  getPhonePrefixNumber(): string {
+    return this.countryDialCode?.includes('+')
       ? this.countryDialCode
       : '+' + this.countryDialCode;
   }
@@ -238,11 +249,62 @@ export class DashboardUserAddComponent implements OnInit {
       );
       this.countryDialCode = country?.dialCode;
     }
+    if (!!this.countrySelected?.length) {
+      this.handleOnGetStatesByCountry();
+    }
   }
 
-  handleOnSelectState(selectedState: string) {}
+  handleOnGetStatesByCountry() {
+    this.stateSelected = '';
+    this.flagService
+      .getStatesByCountry(this.countrySelected)
+      .subscribe((states) => {
+        this.countryStatesData = states.data?.states;
+      }),
+      (error: HttpErrorResponse) => {
+        if (error.status === 403) {
+          // Expiration token
+          this.alertType = AlertType.ALERT_WARNING;
+          this.okText = 'Go to login';
+          this.messageAlert = `Session timeout expiration`;
+          this.descriptionAlert = `Sorry, you session in Warehouse System is expired. Try relogin again and come back.`;
+          this.isExpiredToken = true;
+        } else {
+          console.log('Error Occured during downloading: ', error);
+          this.errorAlertType(error?.error.message);
+        }
+      };
+  }
 
-  handleOnInsertUser() {}
+  handleOnSelectState(selectedState: string) {
+    console.log('handleOnSelectState: ', selectedState);
+    this.stateSelected = selectedState;
+  }
+
+  handleOnInsertUser() {
+    let userData = {
+      fullname: this.validateForm.controls['fullName']?.value,
+      username: this.validateForm.controls['username']?.value.toLowerCase(),
+      email: this.validateForm.controls['email']?.value,
+      secondEmail: this.validateForm.controls['secondEmail']?.value,
+      dateOfBirth: this.validateForm.controls['dateOfBirth']?.value,
+      country: this.validateForm.controls['country']?.value,
+      state: this.validateForm.controls['state']?.value,
+      address: this.validateForm.controls['address']?.value,
+      zipCode: this.validateForm.controls['zipCode']?.value,
+      phonePrefix: this.getPhonePrefixNumber(),
+      phoneNumber: this.validateForm.controls['phoneNumber']?.value,
+      landlineNumber: this.validateForm.controls['landlineNumber']?.value,
+      role: this.validateForm.controls['role']?.value,
+      gender: this.validateForm.controls['gender']?.value,
+    };
+
+    console.log('userData: ', userData);
+  }
+
+  handleOnSelectLandlinePrefix(landlinePrefix: string) {
+    console.log('handleOnSelectLandlinePrefix: ', landlinePrefix);
+  }
 
   handleOnBack() {
     this.router.navigate([`${Pages.WAREHOUSE}/${Pages.DASHBOARD}`]);
--- src/app/services/flag.service.ts
+++ src/app/services/flag.service.ts
@@ -3,18 +3,22 @@ import { Injectable } from '@angular/core';
 import { ExternalApi } from '../shared/enums/external-api-enums';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FlagService {
-
   constructor(private http: HttpClient) {}
 
-  // retrieve all country name and their flags
+  // Retrieve all countries name and theirs flags
   public getDialCodeAndCountryFlag() {
-    return this.http.get<any>(`${ExternalApi.WAREHOUSE_WORLD_COUNTRIES}?returns=currency,flag,unicodeFlag,dialCode`);
+    return this.http.get<any>(
+      `${ExternalApi.WAREHOUSE_WORLD_COUNTRIES}?returns=currency,flag,unicodeFlag,dialCode`
+    );
+  }
+
+  // Get all states by country
+  public getStatesByCountry(country: string) {
+    return this.http.get<any>(
+      `${ExternalApi.WAREHOUSE_STATES_BY_COUNTRY}${country}`
+    );
   }
 }
-
-
-
-
