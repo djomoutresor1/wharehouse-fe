43e907bd663dd5749b5e9db50cb597b96bea5ed1
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2022-07-18 23:48:15 +0200

    Edit user dashbaord

--- src/app/shared/interceptors/common.interceptor.ts
+++ src/app/shared/interceptors/common.interceptor.ts
@@ -42,7 +42,6 @@ export class CommonInterceptor implements HttpInterceptor {
       !request.url.includes(Auth.WAREHOUSE_VERIFY_LINK) &&
       !request.url.includes(Auth.WAREHOUSE_RESET_PASSWORD) &&
       !request.url.includes(Auth.WAREHOUSE_UPLOAD_IMAGE) &&
-      !request.url.includes(Auth.WAREHOUSE_DELETE_IMAGE) &&
       !request.url.endsWith(Auth.WAREHOUSE_VERIFICATION_EMAIL) &&
       !request.url.includes(Auth.WAREHOUSE_ACTIVATE_DESATTIVATE_USER) &&
       !request.url.endsWith(Pages.REGISTER_STEP_2) &&
